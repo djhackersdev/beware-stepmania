007d2e3c7db041b5a144b0da633214e78cc558d6
Tau <tau@nowhere>
2025-07-02 14:52:23 -0400

    Fix sloppy return types

--- src/RageSurfaceUtils.cpp
+++ src/RageSurfaceUtils.cpp
@@ -747,7 +747,7 @@ RageSurface *RageSurfaceUtils::LoadSurface( CString file )
 {
 	RageFile f;
 	if( !f.Open( file ) )
-		return false;
+		return NULL;
 
 	SurfaceHeader h;
 	if( f.Read( &h, sizeof(h) ) != sizeof(h) )
